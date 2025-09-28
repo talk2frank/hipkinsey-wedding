d37f282f9441cfb4aa087c10d2a99518056d429d
Frank Kinsey <frankin_c@hotmail.com>
2025-09-28 16:17:23 +0100

    vite file updated for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hipkinsey-wedding/', // Use relative paths for GitHub Pages may need to change if using a custom domain
+  base: '/', // Use relative paths for GitHub Pages may need to change if using a custom domain
   resolve: {
     alias: [],
   },
